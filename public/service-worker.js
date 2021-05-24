323e23a93b6b0a866552d77695b15a4d75f4fac5
srad25 <suzanne.radlein@gmail.com>
2021-05-24 17:50:02 -0400

    upgade to server

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,13 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
   "/index.html",
   "/manifest.webmanifest",
-  "/style.css",
+  "/styles.css",
   "/index.js",
-  "/db.js"
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png" 
 ];
 
 // install
